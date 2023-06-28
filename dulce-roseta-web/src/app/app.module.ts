93cc67b90c08ba2d95476001b237c484e886e9d8
JoseLuisColomaTormo <colomatormojl@gmail.com>
2023-06-28 13:00:38 +0200

    feat(#3): Creación de componentes iniciales y primeros endpoints en rutas

--- dulce-roseta-web/src/app/app.module.ts
+++ dulce-roseta-web/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { ProductosComponent } from './productos/productos.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { ErrorComponent } from './error/error.component';
+import { FooterComponent } from './footer/footer.component';
+import { ServiciosComponent } from './servicios/servicios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    QuienesSomosComponent,
+    ProductosComponent,
+    ContactoComponent,
+    ErrorComponent,
+    FooterComponent,
+    ServiciosComponent
   ],
   imports: [
     BrowserModule,
---
+++ dulce-roseta-web/src/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:800/api' // Reemplaza con la URL de tu backend de Spring Boot
+};
